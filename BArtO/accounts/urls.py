0341787070fd4a2ccc2a9e43a46609712b8c590d
Zhelan Danev <bespoke91@gmail.com>
2024-11-24 21:09:17 +0200

    Update .gitignore

---
+++ BArtO/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+
+from BArtO.accounts.views import ArtistRegisterView, logout_view, artist_edit, ArtistDeleteView, \
+    ArtistDetailView
+
+urlpatterns = [
+    path('register/', ArtistRegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/<int:pk>/', ArtistDetailView.as_view(), name='artist_details'),  # Профилът
+    path('profile/<int:pk>/edit/', artist_edit, name='artist_edit'),    # Редакция
+    path('profile/<int:pk>/delete/', ArtistDeleteView.as_view(), name='artist_delete'),  # Изтриване
+]
---
+++ secret_key_and_database.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-38^e%u(*l=^)%d486m-p@wva3h-8a4w)vz8jq0t0xz4aceli%l'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "exam_barto",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
